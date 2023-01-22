4ee701096bc36ff3eca7f639052520d70bab937b
Monkey 0xJungle.com <Monkey@0xJungle.com>
2023-01-21 19:58:06 -0500

    switch 3 use case

--- switch.go
+++ switch.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
@@ -16,5 +17,34 @@ func main() {
 	case 6:
 		fmt.Println("Six")
 	}
+	// multiple expressions in the same case statement.
+	switch time.Now().Weekday() {
+	case time.Saturday, time.Sunday:
+		fmt.Println("It's weekend.")
+	default:
+		fmt.Println("It's a weekday.")
+	}
+	//switch without an expression (work as if else statement)
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("It's before noon")
+	default:
+		fmt.Println("It's after noon")
+	}
+	// type switch compares types instead of values.
+	whatAmI := func(i interface{}) {
+		switch t := i.(type) {
+		case bool:
+			fmt.Println(("bool"))
+		case int:
+			fmt.Println("int")
+		default:
+			fmt.Printf("don't know type %T \n", t)
+		}
+	}
+	whatAmI(true)
+	whatAmI(1)
+	whatAmI("meow")
 
 }
